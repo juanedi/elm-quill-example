818eb4e63249ffb0e41ab351e150cbed5d1c064b
Juan Edi <juan@noredink.com>
2018-01-31 19:10:47 -0300

    Added custom 'foo' style to toolbar

--- quill/index.js
+++ quill/index.js
@@ -1,5 +1,19 @@
+var Inline = Quill.import('blots/inline');
+
+class FooBlot extends Inline { }
+FooBlot.blotName = 'foo';
+FooBlot.tagName = 'strong';
+
+Quill.register(FooBlot);
+
 var quill = new Quill('.editor', {
   modules: {
     toolbar: ".editor-toolbar"
   }
+});
+
+var toolbar = quill.getModule('toolbar');
+
+toolbar.addHandler('foo', function(value) {
+  quill.format('foo', value);
 });
