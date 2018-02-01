aca2c06459fc67c6603a4013f75dcedd0456e8dc
Juan Edi <juan@noredink.com>
2018-02-01 11:33:43 -0300

    Instructions on how to run

--- quill/NriEditor.elm
+++ quill/NriEditor.elm
@@ -3,6 +3,15 @@ module NriEditor exposing (view)
 import Html exposing (..)
 
 
+{-
+   TODO:
+
+   - [ ] communicate editor contents to Elm
+   - [ ] preload editor contents based on initial Elm mode
+   - [ ] maybe add attributes to component to enable/disable formatting features?
+-}
+
+
 view : Html msg
 view =
     node "nri-editor" [] []
---
+++ quill/README.md
@@ -0,0 +1,14 @@
+# Elm + Quill with custom element example
+
+## How to use
+
+```
+# builds elm code
+$ ./script/build
+
+# alternatively, builds elm code and watches for changes
+$ ./script/watch
+
+# does what it says on the can
+$ open index.html
+```
